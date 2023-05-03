80ec01fe1cb5a21d757922466a505ff09f02dad4
prodipdev <prodipink@gmail.com>
2023-05-03 23:43:59 +0600

    Added Generate to PDF

--- src/pages/blog/Blog.jsx
+++ src/pages/blog/Blog.jsx
@@ -1,34 +1,54 @@
-import React from "react";
-import { Link, useLoaderData } from "react-router-dom";
+import React, { useRef } from "react";
+import { useLoaderData } from "react-router-dom";
+import Pdf from "react-to-pdf";
 
 const Blog = () => {
   const questions = useLoaderData();
-  console.log(questions);
+  const ref = useRef();
+
+  const generatePdf = () => {
+    const doc = new jsPDF();
+    doc.html(ref.current, {
+      callback: function () {
+        doc.save("code-example.pdf");
+      },
+      html2canvas: { scale: 0.5 },
+    });
+  };
 
   return (
-    <div className="mb-10">
-      <div className="text-center bg-primary py-5 sm:pt-10">
-        <Link to={"/"} className="sm:hidden">
-          <h2 className="text-xl font-bold tracking-wide">JobHunter</h2>
-        </Link>
-        <h2 className="text-xl sm:text-2xl font-semibold">
-          Find Your Question
-        </h2>
-      </div>
-      <div className="px-5 sm:px-10 grid sm:grid-cols-2 gap-5 gap-y-8">
-        {questions.map((question) => (
-          <div
-            key={question.id}
-            className="bg-gray-100 rounded-md overflow-hidden shadow shadow-violet-300"
+    <>
+      <Pdf targetRef={ref} filename="questions.pdf" pageSize="A4">
+        {({ toPdf }) => (
+          <button
+            className="px-3 py-1 bg-rose-500 text-white font-semibold rounded-r-full shadow-md"
+            onClick={toPdf}
           >
-            <h2 className="text-lg sm:text-xl px-5 pt-2 font-semibold tracking-wide bg-gray-400 text-white bg-gradient">
-              {question.question}
-            </h2>
-            <p className="px-5 pt-1 pb-5 text-gray-600">{question.answer}</p>
-          </div>
-        ))}
+            Generate Pdf
+          </button>
+        )}
+      </Pdf>
+      <div className="mb-10" ref={ref}>
+        <div className="text-center bg-primary py-5">
+          <h2 className="text-xl sm:text-2xl font-semibold">
+            Find Your Question
+          </h2>
+        </div>
+        <div className="px-5 sm:px-10 grid sm:grid-cols-2 gap-5 gap-y-8">
+          {questions.map((question) => (
+            <div
+              key={question.id}
+              className="bg-gray-100 rounded-md overflow-hidden shadow shadow-violet-300"
+            >
+              <h2 className="text-lg sm:text-xl px-5 pt-2 font-semibold tracking-wide bg-gray-400 text-white bg-gradient">
+                {question.question}
+              </h2>
+              <p className="px-5 pt-1 pb-5 text-gray-600">{question.answer}</p>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
